e93d65a0876816b6c78e675f06725d1efed4c7e0
coc lay <coc.lay97@gmail.com>
2023-05-23 23:02:37 -0400

    add nav tab

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+
+export default function Resume() {
+    return (
+        <>
+        <h2>Resume</h2>
+        <a href="#">Download My Resume</a>
+
+        <h2>Front-End Proficiencies</h2>
+
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        <p>jQuery</p>
+        <p>React</p>
+        <p>bootstrap</p>
+
+        <h2>Back-End Proficiencies</h2>
+        <p>Nodejs</p>
+        <p>MongoDb</p>
+        <p>MySql</p>
+        </>
+    )
+}
