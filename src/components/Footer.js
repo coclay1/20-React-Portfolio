d55838d82002d124a7cae80993b938acfb221acc
coc lay <coc.lay97@gmail.com>
2023-05-23 22:32:02 -0400

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+export default function Footer() {
+    return (
+        <>
+            <a href="#">(555) 555-5555</a>
+            <a href="#">coc.lay97@gmail.com</a>
+            <a href="#">My github</a>
+        </>
+    )
+}
