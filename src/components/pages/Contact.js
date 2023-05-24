c75e9f3918c3b33a1ec689e7eb3574f8ea6a18a9
coc lay <coc.lay97@gmail.com>
2023-05-23 22:25:35 -0400

    set up about and contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Contact() {
+    return (
+        <div>
+            <form>
+                <input
+                    value={ }
+                    name="name"
+                    onChange={ }
+                    type="name"
+                    placeholder="name"
+                />
+                <input
+                    value={ }
+                    name="email"
+                    onChange={ }
+                    type="email"
+                    placeholder="email"
+                />
+                <input
+                    value={ }
+                    name="message"
+                    onChange={ }
+                    type="message"
+                    placeholder="message"
+                />
+                <button type="button" onClick={}>Submit</button>
+            </form>
+        </div>
+    )
+}
